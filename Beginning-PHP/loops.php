02f542cbaceb13de4000dc13e888c2be8056bf83
Geraldo Hernandez <geraldo.hernandez@outlook.com>
2020-07-12 13:17:10 -0700

    Added feature to sort based on 'field' and 'completion_status'

--- Beginning-PHP/loops.php
+++ Beginning-PHP/loops.php
@@ -2,8 +2,11 @@
 
 include 'list.php';
 
-// this will display todo items, based on being completed or not
-$status = "all";
+/** USER-INPUT: this will display todo items, based on being completed or not **/
+$status = false;
+
+/** USER-INPUT: this will sort a column based on the specified column value **/
+$field = 'priority';
 
 // this will hold the outer array indices that match the $status
 $filter = array();
@@ -12,9 +15,17 @@ foreach($list as $mainKey => $item){
 	// if the given multi-array's "Complete" value matches $status,
 	// then place the array index number to the $filter array
 	if($status === "all" || $item['complete'] === $status){
-		$filter[$mainKey] = $item['priority'];
+		if(isset($field) && isset($item[$field])){
+			// 
+			$filter[$mainKey] = $item[$field];
+		}else{
+			// Add 12 to priority value, to ensure its shown after any due-date values
+			$filter[$mainKey] = $item['priority'] + 12;
+		}
 	}
 }
+
+asort($filter);
 
 echo '<pre>';
 var_dump($filter);
@@ -32,7 +43,7 @@ echo "<th>Complete</th>";
 echo "</tr>";
 
 // Loop through array and display values in an HTML table
-foreach($filter as $id){
+foreach($filter as $id => $value){
 	echo "<tr>";
 	echo "<td>" . $list[$id]['title'] . "</td>";
 	echo "<td>" . $list[$id]['priority'] . "</td>";
