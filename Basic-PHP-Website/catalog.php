f613795b71c6a116aa5b75a63ea7d40ee8cafe4e
Geraldo Hernandez <geraldo.hernandez@outlook.com>
2020-07-12 23:46:11 -0700

    Added header/footer.php files

---
+++ Basic-PHP-Website/catalog.php
@@ -0,0 +1,10 @@
+<?php 
+$pageTitle = "Full Catalog";
+
+include("inc/header.php"); ?>
+
+<div>
+    <h1>Full Catalog</h1>
+</div>
+
+<?php include("inc/footer.php"); ?>
---
+++ Basic-PHP-Website/suggest.php
@@ -0,0 +1,10 @@
+<?php 
+$pageTitle = "Suggest a Media Item";
+
+include 'inc/header.php' ?>
+
+<div class="section page">
+    <h1>Suggest a Media Item</h1>
+</div>
+
+<?php include 'inc/footer.php' ?>
