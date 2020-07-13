de61502ba1a001905d3aaa46e69685b56bb32ddd
Geraldo Hernandez <geraldo.hernandez@outlook.com>
2020-07-13 00:34:40 -0700

    Added nav links to redirect to catalog.php based on  input

--- Basic-PHP-Website/catalog.php
+++ Basic-PHP-Website/catalog.php
@@ -1,6 +1,22 @@
 <?php 
 $pageTitle = "Full Catalog";
 
+$section = null;
+
+// Will only execute the code if "cat" is set
+if(isset($_GET["cat"])){
+    if($_GET["cat"] == "books"){
+        $pageTitle = "Books";
+        $section = "books";
+    }else if($_GET["cat"] == "movies"){
+        $pageTitle = "Movies";
+        $section = "movies";
+    }else if($_GET["cat"] == "music"){
+        $pageTitle = "Music";
+        $section = "music";
+    }
+}
+
 include("inc/header.php"); ?>
 
 <div>
