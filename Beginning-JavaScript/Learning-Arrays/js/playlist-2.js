00fdbebe54bcfd752d7c957a0787732616e69d83
Geraldo Hernandez <31232829+hdezgeraldo@users.noreply.github.com>
2020-07-14 13:29:30 -0700

    Added quiz challenge program to 'Beginning JS' folder

--- Beginning-JavaScript/Learning-Arrays/js/playlist-2.js
+++ Beginning-JavaScript/Learning-Arrays/js/playlist-2.js
@@ -1,16 +1,26 @@
 const playlist = [
-  'So What',
-  'Respect',
-  'What a Wonderful World',
-  'At Last',
-  'Three Little Birds',
-  'The Way You Look Tonight'
+  ['So What', 'Miles Davis', '9:04'],
+  ['Respect', 'Aretha Franklin', '2:45'],
+  ['What a Wonderful World', 'Louis Armstrong', '2:21'],
+  ['At Last', 'Ella Fitzgerald', '4:18'],
+  ['Three Little Birds', 'Bob Marley and the Wailers', '3:01'],
+  ['The Way You Look Tonight', 'Frank Sinatra', '3:21']
 ];
+
+var myArtists = `${playlist[0][1]}, $`
 
 function createListItems( arr ) {
   let items = '';
   for ( let i = 0; i < arr.length; i++ ) {
-    items += `<li>${ arr[i] }</li>`;
+    for( let j = 0; j < arr[i].length; j++){
+      if(j === 0){
+        items += `<li>${ arr[i][j] },`;
+      }else if(j === 1){
+        items += ` by ${ arr[i][j] }`;
+      }else{
+        items += ` - ${ arr[i][j] }</li>`;
+      }
+    }
   }
   return items;
 }
---
+++ Beginning-JavaScript/Learning-Arrays/js/quiz.js
@@ -0,0 +1,20 @@
+// 1. Create a multidimensional array to hold quiz questions and answers
+var quiz_array = [
+  ["What is my first name?", "Geraldo"],
+  ["What is my last name?", "Hernandez"],
+  ["What is my favorite dessert?", "Banana cake"]
+];
+
+// 2. Store the number of questions answered correctly
+var score = 0;
+
+/* 
+  3. Use a loop to cycle through each question
+      - Present each question to the user
+      - Compare the user's response to answer in the array
+      - If the response matches the answer, the number of correctly
+        answered questions increments by 1
+*/
+
+
+// 4. Display the number of correct answers to the user
