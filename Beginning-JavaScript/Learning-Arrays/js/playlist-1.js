9f958058b6088de51daa2c5d35540b083302a01f
Geraldo Hernandez <31232829+hdezgeraldo@users.noreply.github.com>
2020-07-14 12:00:07 -0700

    Searching through an array and display results in HTML page

--- Beginning-JavaScript/Learning-Arrays/js/playlist-1.js
+++ Beginning-JavaScript/Learning-Arrays/js/playlist-1.js
@@ -1,8 +1,33 @@
 const playlist = [
-  'So What',
-  'Respect',
-  'What a Wonderful World',
-  'At Last',
-  'Three Little Birds',
-  'The Way You Look Tonight'
+	'So What',
+	'Respect',
+	'What a Wonderful World',
+	'At Last',
+	'Three Little Birds',
+	'The Way You Look Tonight'
 ];
+
+/* Name: createItemList
+	 Description: this function will loop through an array
+	 and proceed to add the contents to a string. */
+function createItemList(arr){
+	var list = '';
+	for(var i = 0; i < arr.length; i++){
+		list += `<li>${arr[i]}</li>`
+	}
+	return list;
+}
+
+document.querySelector('main').innerHTML = 
+	`<ol>
+		${createItemList(playlist)}
+	</ol>
+	`
+
+// testing 'indexOf()' function on a non-existing value
+const people = [ 'Maria', 'Alyssa', 'Toni', 'Lee', 'Reggie' ];
+let personIndex = people.indexOf('Maria');  // 0
+
+if ( personIndex !== -1) { // it's ok because we're checking for -1 only
+	console.log(`${people[personIndex]} is in the array at index ${personIndex}.`);
+}
--- Beginning-JavaScript/Learning-Arrays/js/search-list.js
+++ Beginning-JavaScript/Learning-Arrays/js/search-list.js
@@ -1,3 +1,15 @@
 const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
-const search = prompt('Search for a product.');
+var search = prompt('Search for a product.');
 let message;
+
+
+if(!search){
+// Obtain message from user and display search results
+	message = `<ul><strong>In stock:</strong> ${inStock.join(', ')}</ul>`
+}else if(inStock.includes(search.toLowerCase()) ){
+	message = `Yes, we have ${search}. It's #${inStock.indexOf(search.toLowerCase()) + 1} on the list!`;
+}else{
+	message = `No, we don't have that item in stock`;
+}
+
+document.querySelector('main').innerHTML = `<ul>${message}</ul>`
